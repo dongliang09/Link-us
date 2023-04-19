01b63e0232e0e99a7608239878b51107d5c3a62f
Dongliang Li <dongliang14@gmail.com>
2023-04-19 00:07:26 -0700

    post route update delete

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -8,13 +8,17 @@ post_routes = Blueprint('posts', __name__)
 
 @post_routes.route('/')
 def allPosts():
+  """
+  Query all posts and return them in a list
+  """
   posts = Post.query.all()
   return [post.to_dict() for post in posts]
 
-@post_routes.route('/', method=['POST'])
+@post_routes.route('/', methods=['POST'])
 def createPost():
-  posts = Post.query.all()
-
+  """
+  return new post if all properties pass validation of post
+  """
   form = PostForm()
   form['csrf_token'].data = request.cookies['csrf_token']
   if form.validate_on_submit():
@@ -25,8 +29,31 @@ def createPost():
     db.session.add(new_post)
     db.session.commit()
     return new_post.to_dict()
+  return "Bad Data"
 
-  if form.errors:
-    return "Bad Data"
+@post_routes.route('/<int:id>', methods=['PUT'])
+def updatePost(id):
+  """
+  return edited post if all properties pass validation of post
+  """
+  form = PostForm()
+  form['csrf_token'].data = request.cookies['csrf_token']
+  if form.validate_on_submit():
+    foundPost = Post.query.get(id)
+    foundPost.content = form.data['content'],
+    db.session.commit()
+    return foundPost.to_dict()
+  return "Bad Data"
 
-  return "create post default"
+@post_routes.route('/<int:id>')
+def removePost(id):
+  """
+  Delete selected post if found
+  """
+  foundPost = Post.query.get(id)
+  if foundPost:
+    db.session.delete(foundPost)
+    # return "post deleted"
+    return foundPost.to_dict()
+  else:
+    return "not found"
