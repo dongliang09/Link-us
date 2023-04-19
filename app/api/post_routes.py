cfab622f4ed5bf329f085d75ee2ae0ebef7c357d
Dongliang Li <dongliang14@gmail.com>
2023-04-19 09:19:10 -0700

    post route return object format reponse if any error

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, request
-from app.models import db, User, Post
+from app.models import db, Post
 from flask_login import current_user, login_required
 from .auth_routes import validation_errors_to_error_messages
 from ..forms import PostForm
@@ -7,6 +7,7 @@ from ..forms import PostForm
 post_routes = Blueprint('posts', __name__)
 
 @post_routes.route('/')
+@login_required
 def allPosts():
   """
   Query all posts and return them in a list
@@ -14,7 +15,17 @@ def allPosts():
   posts = Post.query.all()
   return [post.to_dict() for post in posts]
 
+@post_routes.route('/<int:id>')
+@login_required
+def onePost(id):
+  """
+  Query selected post and return it as dictionary
+  """
+  post = Post.query.get(id)
+  return post.to_dict()
+
 @post_routes.route('/', methods=['POST'])
+@login_required
 def createPost():
   """
   return new post if all properties pass validation of post
@@ -29,23 +40,28 @@ def createPost():
     db.session.add(new_post)
     db.session.commit()
     return new_post.to_dict()
-  return "Bad Data"
+  return {'error':"Bad Data"}
 
 @post_routes.route('/<int:id>', methods=['PUT'])
+@login_required
 def updatePost(id):
   """
   return edited post if all properties pass validation of post
   """
   form = PostForm()
   form['csrf_token'].data = request.cookies['csrf_token']
-  if form.validate_on_submit():
-    foundPost = Post.query.get(id)
-    foundPost.content = form.data['content'],
-    db.session.commit()
-    return foundPost.to_dict()
-  return "Bad Data"
+  foundPost = Post.query.get(id)
+  if foundPost:
+    if form.validate_on_submit():
+      foundPost.content = form.data['content']
+      db.session.commit()
+      return foundPost.to_dict()
+    return {'error':"Bad Data"}
+  else:
+    return {'error':"post not found"}
 
-@post_routes.route('/<int:id>')
+@post_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
 def removePost(id):
   """
   Delete selected post if found
@@ -53,7 +69,7 @@ def removePost(id):
   foundPost = Post.query.get(id)
   if foundPost:
     db.session.delete(foundPost)
-    # return "post deleted"
-    return foundPost.to_dict()
+    db.session.commit()
+    return {"message":"post deleted"}
   else:
-    return "not found"
+    return {'error':"post not found"}
