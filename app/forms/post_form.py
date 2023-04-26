094d39d1c468e9f6e17114651d91377b3c069f33
Dongliang Li <dongliang14@gmail.com>
2023-04-25 23:22:33 -0700

    add some post and comment validate on form

--- app/forms/post_form.py
+++ app/forms/post_form.py
@@ -3,5 +3,11 @@ from wtforms import StringField
 from wtforms.validators import DataRequired, ValidationError
 from app.models import Post
 
+def post_length(form, field):
+    # Checking the length of password
+    post = field.data
+    if len(post) > 255:
+        raise ValidationError('Post only accepts a maximum of 255 characters.')
+
 class PostForm(FlaskForm):
-    content = StringField('content', validators=[DataRequired()])
+    content = StringField('content', validators=[DataRequired(), post_length])
