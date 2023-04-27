bb6fd45e9719825213b04e88a98fd005aebd35c1
Dongliang Li <dongliang14@gmail.com>
2023-04-27 15:09:29 -0700

    education modal 2

--- react-app/src/components/ProfilePage/index.js
+++ react-app/src/components/ProfilePage/index.js
@@ -1,34 +1,41 @@
 import React, { useEffect } from "react";
-import { useParams } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
+import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { thunkGetAllUsers } from "../../store/user";
 import { thunkGetAllEducations } from "../../store/education";
-import default_education from "./default_education.PNG"
+import OpenModalButton from "../OpenModalButton";
+import AddEducationModal from "./addEducationModal";
+import default_education from "./default_education.PNG";
+
 
 function ProfilePage() {
   const { userId } = useParams();
   const sesssionUser = useSelector(state=>state.session.user);
+  const profileUser = Object.values(useSelector(state=>state.users.allUsers)).find(element=>element.id === Number(userId))
   const currUserEducations = Object.values(useSelector(state=>state.educations.allEducations))
     .filter((element)=>element.user_id === Number(userId));
   const dispatch = useDispatch();
 
   useEffect(()=> {
-    dispatch(thunkGetAllEducations())
+    dispatch(thunkGetAllUsers());
+    dispatch(thunkGetAllEducations());
   },[dispatch])
 
   return (
     <div className="bg-gray pad-t-150rem">
       <div className="flx-col gap-15p width-max-1000p mrg-lr-auto ">
         <div className="bg-white borderR-15p pad-15p">
-          <div>Banner</div>
-          <div>basic info</div>
+          <div className="fontS-175rem pad-15p">{profileUser?.firstName} {profileUser?.lastName}</div>
         </div>
         <div className="flx-col bg-white borderR-15p pad-lr-150rem fontS-115rem">
           <div className="flx flx-jc-sb pad-lr-15p pad-t-150rem pad-b-050rem">
             <span className="fontS-135rem">Education</span>
             <div>
-              <button className="bg-white border-0p fontS-115rem borderR-50per bg-gray-hover">
-                <i className="fas fa-plus"></i>
-              </button>
+              {Number(userId) === sesssionUser.id && <OpenModalButton
+                modalComponent={<AddEducationModal />}
+                buttonText={<i className="fas fa-plus"></i>}
+                customizeStyle="bg-white border-0p fontS-115rem borderR-50per bg-gray-hover width-250rem hgt-250rem"
+              />}
             </div>
           </div>
           {currUserEducations.map((education, idx)=><div key={idx}>
